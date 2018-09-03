e4adad194246c02f4191cb2efa66108df6adcec2
rahul9999 <36025572+rahul9999xda@users.noreply.github.com>
2018-09-03 23:52:17 +0530

    Fix Shims issue

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,12 +155,11 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 TARGET_LD_SHIM_LIBS := \
     /system/lib/libandroid.so|libshim_ril.so \
     /system/lib/libmdmcutback.so|libqsap_shim.so \
+    /system/lib/libmot_sensorlistener.so|libsensor.so \
     /system/lib/libjustshoot.so|libshims_camera.so \
-    /system/lib/libmot_sensorlistener.so|libshims_sensorlistener.so \
-    /system/vendor/lib/libmmcamera_wavelet_lib.so|libshim_atomic.so \
-    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
-    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
     /system/vendor/lib/libmmcamera_wavelet_lib.so|libc_util.so \
+    /system/vendor/lib/libmmqjpeg_codec.so|libboringssl-compat.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
     /system/lib/libfacenet.so|libprotobuf-cpp-haxx.so
 
 # Wifi
